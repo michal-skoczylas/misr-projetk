dd9e5db36debe6f5b8cbe99c714c4e0dddf5516e
Jan Rębacz <janrebacz@vp.pl>
2025-04-29 20:43:44 +0200

    Dynamics pt.2

--- NewtonEuler_Dynamics.m
+++ NewtonEuler_Dynamics.m
@@ -1,4 +1,5 @@
 syms theta1 theta2 l1 l2 l3 l4 l5 d3 m1 m2 m3 Ixx1 Iyy1 Izz1 Ixx2 Iyy2 Izz2 Ixx3 Iyy3 Izz3;
+syms theta1bis theta2bis d3bis;
 
 R01 = [cosd(theta1) -sind(theta1) 0 ; sind(theta1) cosd(theta1) 0 ; 0 0 1];
 
@@ -23,10 +24,15 @@ I3 = [Ixx3 0 0 ; 0 Iyy3 0 ; 0 0 Izz3];
 
 
 D1 = transpose(Jv1)*Jv1*m1 + transpose(Jw1)*R01*I1*transpose(R01)*Jw1 ; 
-simplify(D1)
+simplify(D1);
 
 D2 = transpose(Jv2)*Jv2*m2 + transpose(Jw2)*R02*I2*transpose(R02)*Jw2 ; 
 simplify(D2)
 
 D3 = transpose(Jv3)*Jv3*m3 + transpose(Jw3)*R03*I3*transpose(R03)*Jw3 ; 
-simplify(D3)
+simplify(D3);
+
+D= D1+D2+D3;
+simplify(D)
+
+qbis = [theta1bis ; theta2bis ; d3bis];
