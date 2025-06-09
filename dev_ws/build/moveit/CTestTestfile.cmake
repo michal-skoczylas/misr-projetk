af922f30d2c0deeb8e4b5e4ddcd943c7f34cbda0
michal-skoczylas <privmichalskoczylas@gmail.com>
2025-06-09 18:11:39 +0000

    zmianki

--- dev_ws/build/moveit/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /misr-projetk/dev_ws/src/moveit
-# Build directory: /misr-projetk/dev_ws/build/moveit
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- dev_ws/build/scara_moveit/colcon_command_prefix_build.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/misr-projetk/dev_ws/install/robot_publisher/share/robot_publisher/package.sh"
